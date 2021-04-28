85a753f9517eef7d3742d5cc96db61a76e9960e3
mr-r4ms0n <71031286+mr-r4ms0n@users.noreply.github.com>
2021-04-28 18:50:00 -0500

    sobre esta aplica tus cambios

--- CitaPLUS/src/formularios_Registros/FormPacientes.java
+++ CitaPLUS/src/formularios_Registros/FormPacientes.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package formularios;
+package formularios_Registros;
 
 import RSMaterialComponent.RSTextFieldOne;
 import java.awt.Color;
